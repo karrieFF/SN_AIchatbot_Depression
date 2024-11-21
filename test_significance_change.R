da209e7800c463176fd1d964ee3fa99b992cda6d
Lingyi <fly.karrie@gmail.com>
2024-11-21 13:19:08 -0700

    rm file from main branch

--- test_significance_change.R
+++
@@ -1,8 +0,0 @@
-
-#finally, we can determine if step counts can be significnatly changed or not
-
-# Perform paired t-test
-t_test_result <- t.test(original_data$Baseline_PA, original_data$Follow_up_PA, paired = TRUE)
-
-# Print the results
-print(t_test_result)
